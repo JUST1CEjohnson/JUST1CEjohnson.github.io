d667fb354ada1bf5a67b355b4bf0e94b47b11da3
JJ <continuouscloudconsulting@gmail.com>
2022-12-19 22:24:06 -0500

    more svelteing

---
+++ docs/_app/immutable/chunks/4-57f71732.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/sverdle/_page.svelte-9f03e7eb.js";const e=!0;export{t as component,e as server};
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static"; 
+// was "@sveltejs/adapter-auto"
 
-/** @type {import('@sveltejs/kit').Config} */
+const dev = "production" === "development";
+
+/** @type {import(""@sveltejs/kit").Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter({
+			fallback: '200.html',
+            pages: "docs",
+            assets: "docs"
+        }),
+    }
 };
 
 export default config;
